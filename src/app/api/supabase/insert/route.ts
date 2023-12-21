61c477fe2da83e2609ae5e3d02c686606bdcab9d
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-12-21 22:06:52 +0400

    changes

--- src/app/api/supabase/insert/route.ts
+++ src/app/api/supabase/insert/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 
-export const supabase = createClient(
+const supabase = createClient(
   'https://kxzjxqtbievffsoxdeij.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4emp4cXRiaWV2ZmZzb3hkZWlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMzE3MDgxNywiZXhwIjoyMDE4NzQ2ODE3fQ.-bef-Fs-4gEN_oRDm5VIWMaRybVsu82OOTj3X6jEqLU'
 );
--- src/app/api/supabase/select/route.ts
+++ src/app/api/supabase/select/route.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-export const supabase = createClient(
+const supabase = createClient(
   'https://kxzjxqtbievffsoxdeij.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4emp4cXRiaWV2ZmZzb3hkZWlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMzE3MDgxNywiZXhwIjoyMDE4NzQ2ODE3fQ.-bef-Fs-4gEN_oRDm5VIWMaRybVsu82OOTj3X6jEqLU'
 );
